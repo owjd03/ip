95dc1f0259668d90c5c9a1bc7907238972d9630e
Jun Duan <e1398588@u.nus.edu>
2025-08-20 19:05:49 +0800

    Add Echo

--- src/main/java/Jason.java
+++ src/main/java/Jason.java
@@ -1,13 +1,33 @@
+import java.util.Scanner;
+
 public class Jason {
-    public static void main(String[] args) {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
 		String name = "Jason";
+		String input = "";
+		Boolean exit = false;
 
 		System.out.println("________________________________________");
 		System.out.println("Hello! I'm " + name);
 		System.out.println("What can I do for you?");
 		System.out.println("________________________________________");
 
+		while (!exit) {
+			input = scanner.nextLine();
+
+			if (input.equals("bye")) {
+				exit = true;
+			} else {
+				System.out.println("________________________________________");
+				System.out.println("     " + input);
+				System.out.println("________________________________________");
+			}
+		}
+
+		System.out.println("________________________________________");
 		System.out.println("Bye. Hope to see you again soon!");
 		System.out.println("________________________________________");
-    }
+
+		scanner.close();
+	}
 }
