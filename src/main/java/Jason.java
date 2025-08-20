83f80bfc95c527090c44d7bc4e5483fbc4f28d3e
Jun Duan <e1398588@u.nus.edu>
2025-08-20 19:53:04 +0800

    add Mark as Done

--- src/main/java/Jason.java
+++ src/main/java/Jason.java
@@ -7,7 +7,7 @@ public class Jason {
 		String name = "Jason";
 		String input = "";
 		Boolean exit = false;
-		ArrayList<String> storage = new ArrayList<>();
+		ArrayList<Task> storage = new ArrayList<>();
 
 		System.out.println("________________________________________");
 		System.out.println("Hello! I'm " + name);
@@ -19,17 +19,40 @@ public class Jason {
 
 			if (input.equals("bye")) {
 				exit = true;
+
 			} else if(input.equals("list")) {
 				System.out.println("________________________________________");
-				for (int i = 0;i<storage.size();i++) {
-					System.out.println("	" + (i + 1) + ". " + storage.get(i));
+				System.out.println("	Here are the tasks in your list:");
+				for (int i = 0; i < storage.size(); i++) {
+					System.out.println("	" + (i + 1) + ". " + storage.get(i).toString());
 				}
 				System.out.println("________________________________________");
+
+			} else if(input.startsWith("mark")) {
+				int number = Integer.parseInt(input.substring(5)) - 1;
+				storage.get(number).markDone();
+
+				System.out.println("________________________________________");
+				System.out.println("	Nice! I've marked this task as done:");
+				System.out.println("		" + storage.get(number).toString());
+				System.out.println("________________________________________");
+
+			} else if(input.startsWith("unmark")) {
+				int number = Integer.parseInt(input.substring(7)) - 1;
+				storage.get(number).markNotDone();
+
+				System.out.println("________________________________________");
+				System.out.println("	OK, I've marked this task as not done yet:");
+				System.out.println("	   " + storage.get(number).toString());
+				System.out.println("________________________________________");
+
 			} else {
 				System.out.println("________________________________________");
 				System.out.println("	added: " + input);
 				System.out.println("________________________________________");
-				storage.add(input);
+
+				Task temp = new Task(input);
+				storage.add(temp);
 			}
 		}
 
@@ -39,4 +62,37 @@ public class Jason {
 
 		scanner.close();
 	}
+}
+
+// Task class to handle tasks
+class Task {
+	private String contents;
+	private boolean done;
+
+	public Task(String contents) {
+		this.contents = contents;
+		this.done = false;
+	}
+
+	public String getContents() {
+		return this.contents;
+	}
+
+	public void markDone() {
+		this.done = true;
+	}
+
+	public void markNotDone() {
+		this.done = false;
+	}
+
+	public String getDone() {
+		return this.done ? "[X]" : "[ ]";
+	}
+
+	@Override
+	public String toString() {
+		return getDone() + " " + contents;
+	}
+
 }
