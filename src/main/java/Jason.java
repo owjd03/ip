62a4a56806c720c4ebe85b6dc10360e36478f2f3
Jun Duan <e1398588@u.nus.edu>
2025-08-20 16:36:44 +0800

    Add Rename, Greet, Exit

---
+++ src/main/java/Jason.java
@@ -0,0 +1,13 @@
+public class Jason {
+    public static void main(String[] args) {
+		String name = "Jason";
+
+		System.out.println("________________________________________");
+		System.out.println("Hello! I'm " + name);
+		System.out.println("What can I do for you?");
+		System.out.println("________________________________________");
+
+		System.out.println("Bye. Hope to see you again soon!");
+		System.out.println("________________________________________");
+    }
+}
