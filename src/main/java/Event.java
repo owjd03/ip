eeb6196dd620e80cc5b4cee8707e94659d6cd9f1
Jun Duan <e1398588@u.nus.edu>
2025-08-27 22:37:09 +0800

    Added get methods for Deadline class and Event class

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -8,6 +8,14 @@ public class Event extends Task {
         this.to = to;
     }
 
+    public String getTo() {
+        return this.to;
+    }
+
+    public String getFrom() {
+        return this.from;
+    }
+
     @Override
     public String toString() {
         return "[E]" + super.toString() + " (from: " + this.from + " to: " + this.to + ")";
