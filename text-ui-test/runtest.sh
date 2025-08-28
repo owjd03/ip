e6488f7e6e81c872119677b0334f43644a2ed80d
Jun Duan <e1398588@u.nus.edu>
2025-08-28 14:30:49 +0800

    Update the rounding

--- text-ui-test/runtest.sh
+++ text-ui-test/runtest.sh
@@ -13,14 +13,14 @@ then
 fi
 
 # compile the code into the bin folder, terminates if error occurred
-if ! javac -cp ../src/main/java -Xlint:none -d ../bin ../src/main/java/*.java
+if ! javac -cp ../src/main/java -Xlint:none -d ../bin ../src/main/java/jason/*.java ../src/main/java/jason/*/*.java
 then
     echo "********** BUILD FAILURE **********"
     exit 1
 fi
 
 # run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
-java -classpath ../bin Jason < input.txt > ACTUAL.TXT
+java -classpath ../bin jason.Jason < input.txt > ACTUAL.TXT
 
 # compare the output to the expected output
 diff ACTUAL.TXT EXPECTED.TXT
