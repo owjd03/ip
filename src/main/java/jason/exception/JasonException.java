a394dd2fc8636f93755b5a77d322bb7335ae2fbb
Jun Duan <e1398588@u.nus.edu>
2025-08-30 13:00:33 +0800

    Add javaDoc comments for jasonException.java

--- src/main/java/jason/exception/JasonException.java
+++ src/main/java/jason/exception/JasonException.java
@@ -1,6 +1,13 @@
 package jason.exception;
 
+
 public class JasonException extends Exception {
+
+    /**
+     * Constructs a new JasonException with the specified error message.
+     *
+     * @param message The String message causing the exception
+     */
     public JasonException(String message) {
         super(message);
     }
