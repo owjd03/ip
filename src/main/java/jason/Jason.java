aa2dc1fd5f6c91d478d2cff877d3dec87d2caa50
Jun Duan <e1398588@u.nus.edu>
2025-09-03 19:53:39 +0800

    Add GUI interface and refactor Jason.java and Ui.java to handle GUI class

--- src/main/java/jason/Jason.java
+++ src/main/java/jason/Jason.java
@@ -13,17 +13,17 @@ public class Jason {
 	private TaskList taskList;
 	private Storage storage;
 	private Ui ui;
+	private static boolean isFirstIteration = true;
 
 	/**
 	 * Construct a new Jason instance.
 	 * Intializes the UI, Storage and load files from exiting file.
 	 * If loading fails, create new empty task list.
 	 *
-	 * @param filePath The file path where the tasks are stored in the hard drive.
 	 */
-	private Jason(String filePath) {
+	public Jason() {
 		this.ui = new Ui("Jason");
-		this.storage = new Storage(filePath);
+		this.storage = new Storage("./data/Jason.txt");
 		try {
 			this.taskList = new TaskList(storage.load());
 		} catch (Exception e) {
@@ -32,72 +32,48 @@ public class Jason {
 		}
 	}
 
-	/**
-	 * Creates a new Jason instance and invokes the run function to start the application.
-	 *
-	 * @param args command line argument.
-	 */
+	public String getResponse(String input) {
+		try {
+			if (isFirstIteration) {
+				isFirstIteration = false;
+				return ui.showStart();
 
-	public static void main(String[] args) {
-		new Jason("./data/Jason.txt").run();
-	}
+			} else if (input.equals("bye")) {
+				return exitJason();
 
-	/**
-	 * Runs the main loop.
-	 * Reads the user input and process command until the user chooses to exit.
-	 * Process commands include list, mark, unmark, todo, deadline, event and delete.
-	 *
-	 */
-	public void run () {
-		String input = "";
-		boolean exit = false;
+			} else if (input.equals("list")) {
+				return list();
 
-		ui.showStart();
+			} else if (input.startsWith("mark")) {
+				return markTask(input, true);
 
-		while (!exit) {
-			input = ui.read();
+			} else if (input.startsWith("unmark")) {
+				return markTask(input, false);
 
-			try {
-				if (input.equals("bye")) {
-					exit = true;
+			} else if (input.startsWith("todo")) {
+				return todoAction(input);
 
-				} else if (input.equals("list")) {
-					list();
+			} else if (input.startsWith("deadline")) {
+				return deadlineAction(input);
 
-				} else if (input.startsWith("mark")) {
-					markTask(input, true);
+			} else if (input.startsWith("event")) {
+				return eventAction(input);
 
-				} else if (input.startsWith("unmark")) {
-					markTask(input, false);
+			} else if (input.startsWith("delete")) {
+				return deleteAction(input);
 
-				} else if (input.startsWith("todo")) {
-					todoAction(input);
+			} else if (input.startsWith("find")) {
+				return findAction(input);
 
-				} else if (input.startsWith("deadline")) {
-					deadlineAction(input);
-
-				} else if (input.startsWith("event")) {
-					eventAction(input);
-
-				} else if (input.startsWith("delete")) {
-					deleteAction(input);
-
-				} else if (input.startsWith("find")) {
-					findAction(input);
-
-				} else {
-					// invalid structure
-					throw new JasonException("I am extremely sorry but give me a todo, deadline or event ૮(˶ㅠ︿ㅠ)ა");
-				}
-			} catch (JasonException e) {
-				ui.showError(e.getMessage());
-			} catch (Exception e) {
-				ui.showError("Something went wrong: " + e.getMessage());
+			} else {
+				// invalid structure
+				return "I am extremely sorry but give me a todo, deadline or event ૮(˶ㅠ︿ㅠ)ა";
 			}
+		} catch (JasonException e) {
+			return e.getMessage();
+		} catch (Exception e) {
+			return "Something went wrong: " + e.getMessage();
 		}
-
-		this.ui.showEnd();
-		this.ui.close();
 	}
 
 	/**
@@ -110,16 +86,17 @@ public class Jason {
 		try {
 			storage.save(taskList.getAllTasks());
 		} catch (Exception e) {
-			this.ui.showError("Something went wrong: " + e.getMessage());
+			System.out.println("Something went wrong: " + e.getMessage());
 		}
 	}
 
 	/**
 	 * Calls the showList function in ui to display the full list.
 	 *
+	 * @return Returns the string input for the list
 	 */
-	public void list() {
-		ui.showList(taskList);
+	public String list() {
+		return ui.showList(taskList);
 	}
 
 	/**
@@ -128,9 +105,10 @@ public class Jason {
 	 *
 	 * @param input The user input string containing the task index.
 	 * @param mark If true mark complete, if false mark uncomplete.
+	 * @return Returns the string input for the markTask
 	 * @throws JasonException If the task index is invalid or parsing fails.
 	 */
-	public void markTask(String input, Boolean mark) throws JasonException {
+	public String markTask(String input, Boolean mark) throws JasonException {
 		int cut = 0;
 		if (mark) {
 			cut = 5;
@@ -142,8 +120,8 @@ public class Jason {
 
 		taskList.markTask(taskIndex, mark);
 		Task temp = taskList.getTask(taskIndex);
-		ui.showMarked(temp, mark);
 		saveToStorage();
+		return ui.showMarked(temp, mark);
 
 	}
 
@@ -152,15 +130,15 @@ public class Jason {
 	 * Parses the input, add it to the taskList, show confirmation and save to storage
 	 *
 	 * @param input The user input string to create a Todo task.
+	 * @return Returns the string input for the todoAction
 	 * @throws JasonException If Todo format is invalid or parsing fails.
 	 */
-	public void todoAction(String input) throws JasonException{
+	public String todoAction(String input) throws JasonException{
 
 		Todo todo = Parser.parseTodo(input);
 		taskList.add(todo);
-		ui.showAddTask(todo, taskList.size());
 		saveToStorage();
-
+		return ui.showAddTask(todo, taskList.size());
 
 	}
 
@@ -169,15 +147,16 @@ public class Jason {
 	 * Parses the input, add it to the taskList, show confirmation and save to storage
 	 *
 	 * @param input The user input string to create a Deadline task.
+	 * @return Returns the string input for the deadlineAction
 	 * @throws JasonException If Todo format is invalid or parsing fails.
 	 */
-	public void deadlineAction(String input) throws JasonException{
+	public String deadlineAction(String input) throws JasonException{
 
 		Deadline deadline = Parser.parseDeadline(input);
 		taskList.add(deadline);
-		ui.showAddTask(deadline, taskList.size());
-
 		saveToStorage();
+		return ui.showAddTask(deadline, taskList.size());
+
 	}
 
 	/**
@@ -185,42 +164,50 @@ public class Jason {
 	 * Parses the input, add it to the taskList, show confirmation and save to storage
 	 *
 	 * @param input The user input string to create a Event task.
+	 * @return Returns the string input for the eventAction
 	 * @throws JasonException If Event format is invalid or parsing fails.
 	 */
-	public void eventAction(String input) throws JasonException{
+	public String eventAction(String input) throws JasonException{
 
 		Event event = Parser.parseEvent(input);
 		taskList.add(event);
-		ui.showAddTask(event, taskList.size());
-
 		saveToStorage();
+		return ui.showAddTask(event, taskList.size());
+
 	}
 
 	/**
 	 * Parses the input, delete it from the task list, show confirmation and save to storage
 	 *
 	 * @param input The user input string containing task index to delete
+	 * @return Returns the string input for the deleteAction
 	 * @throws JasonException If task index is invalid or parsing fails.
 	 */
-	public void deleteAction(String input) throws JasonException{
+	public String deleteAction(String input) throws JasonException{
 		int taskIndex = Parser.parseIndex(input, 6) - 1;
 
 
 		Task delete = taskList.delete(taskIndex);
-		ui.showDeleteTask(delete, taskList.size());
-
 		saveToStorage();
+		return ui.showDeleteTask(delete, taskList.size());
+
+
 	}
 
 	/**
 	 * Parses the input, find it from the task list, show results
 	 *
 	 * @param input The user input string containing the keyword to find
+	 * @return Returns the string input for the findAction
 	 * @throws JasonException If task is not given
 	 */
-	public void findAction(String input) throws JasonException{
+	public String findAction(String input) throws JasonException{
 		String keyword = Parser.parseString(input);
 		ArrayList<Task> tasks = taskList.findTask(keyword);
-		ui.showFindTask(tasks);
+		return ui.showFindTask(tasks);
+	}
+
+	public String exitJason() {
+		return ui.showEnd();
 	}
 }
